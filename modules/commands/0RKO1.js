7a0432af60c994cbdfb276aa5ca8761dca8a4957
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-04-10 22:25:49 +0545

    Create 0RKO1.js

---
+++ modules/commands/0RKO1.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "#OWNER",
+    version: "1.0.1",
+  hasPermssion: 0,
+  credits: "Candy", 
+  description: "no prefix",
+  commandCategory: "No command marks needed",
+  usages: "...",
+    cooldowns: 1, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("owner") ||
+     react.includes("Owner") || 
+react.includes("ko hau tmi")) {
+    var msg = {
+        body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦❏NAME:- HIMAL PAUDEL                                               ❏Age:-AGE DOENS'T MATTTER IN LOVE 💋           ❏WHATSAPP NO:-Ooops! Forgot                       ❏in a relationship with:-Ye bat baatayi nahi jaati😒najar lag jati hai                                              ❏Facebook name/Id link :(RKO BRO)https://www.facebook.com/profile.php?id=100084573213860                               ❏This bot name :- Chota gulab😘                            ❏Okay bye😹✦\n\n☞\n\n★★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 :  ✦RKO BRO`",
+        attachment: fs.createReadStream(__dirname + `/noprefix/himal.jpg`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+        }
