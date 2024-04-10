b29b04ef8a4d9ad4c7f3fa9388e565ea0d7a313e
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-04-10 17:17:05 +0545

    Update bing.js

--- modules/commands/bing.js
+++ modules/commands/bing.js
@@ -1,46 +1,44 @@
-const axios = require('axios');
-const fs = require('fs-extra');
-const path = require('path');
-
 module.exports.config = {
-    name: "bing",
-    version: "1.0",
-    credits: "Aadi Gupta",
-    hasPermssion: 2,
-    description: "Generate images by Dalle-3 AI",
-    commandCategory: "download",
-    usages: "[text] \nJamon [A 17/18/19 years old boy/girl watching football match on tv and written Aadi and 69 on the back of his Dress , 4k]",
-    cooldowns: 5
-  };
+  name: "dalle",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "Nayan",
+  description: "",
+  commandCategory: "Image",
+  usages: "dalle cat",
+  cooldowns: 5
+};
 
-module.exports.run = async function ({ api, event, args }) {
-  const prompt = event.messageReply?.body.split("dalle")[1] ||  args.join(" ");
-  if (!prompt) {
-   return api.sendMessage("❌| Wrong Formet .✅ | Use 17/18 years old boy/girl watching football match on tv and written Aadi and 69 on the back of his Dress , 4k",event.threadID,event.messageID);
-  }
-    try {
-      const w = await api.sendMessage("Waiting few seconds baby < 😽", event.threadID);
-  
-const response = await axios.get(`https://all-image-genator-d1p.onrender.com/dipto/dalle?prompt=${prompt}&key=dipto008&cookies=1xx-CWi5lgL8CItH5p0YiIlS9N25oFEii5TAU7XZCFVH5qNkZVnsPgw2Q8-ICPcG-g755akN5DObO7PpgBJ2ILhvk8qDDc4J1IUAdxDwriZKO2MdaLOIX_VTMaooFIAiUzHMwAPTkOM7zwsym74buHdvPgpiUhTvAqny167B9ei2_0TtgH74qT3jyQEhOQePW6MHr_SHGSP3JuHAqnpybnx-4XvwQwRRWcASQlz6nYpo`)
-      const data = response.data.imgUrls;
-      if (!data || data.length === 0) {
-        api.sendMessage("Empty response or no images generated.",event.threadID,event.messageID);
-      }
-      const diptoo = [];
-      for (let i = 0; i < data.length; i++) {
-        const imgUrl = data[i];
-        const imgResponse = await axios.get(imgUrl, { responseType: 'arraybuffer' });
-        const imgPath = path.join(__dirname, 'dalle', `${i + 1}.jpg`);
-        await fs.outputFile(imgPath, imgResponse.data);
-        diptoo.push(fs.createReadStream(imgPath));
-      }
-      await api.unsendMessage(w.messageID);
-      await api.sendMessage({
-  body: `✅ |Your pic Generated <😘`,
-        attachment: diptoo
-      },event.threadID, event.messageID);
-    } catch (error) {
-      console.error(error);
-      await api.sendMessage(`Generation failed!\nError: ${error.message}`,event.threadID, event.messageID);
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios");
+    const fs = require("fs-extra");
+    const prompt = args.join(" ");
+    const key = this.config.credits;
+    if (!prompt) return api.sendMessage('use : /bing cat', event.threadID, event.messageID); 
+
+    const rndm = ['1RP2NejFdSr8cyHso92Lcgumrl89GcFn2cvYl9AgLEdVoKOyBC4lAAepmXdgsKH-j5uH0UcQ6yxMYJIe76TzfYhfp78pB2_YyyZnGe6MbrDFgG9smmGLpqrFI1gLOvH90IpJ7sKbAnFSRSqmIWOwpp6TMIM8SXnjJJDe7QcDk4UjpgFobgzw-HEeKdStAklLf1TMS6pEgWgrXV9_tGm5-7Q'];  //input cooki here
+    var cookie = rndm[Math.floor(Math.random() * rndm.length)];
+
+    const res = await axios.get(`https://bing-api-5dpl.onrender.com/bing-img?key=${key}&cookie=${cookie}&prompt=${encodeURIComponent(prompt)}`);
+
+    console.log(res.data);
+    const data = res.data.result;
+    const numberSearch = data.length;
+    var num = 0;
+    var imgData = [];
+    for (var i = 0; i < parseInt(numberSearch); i++) {
+        let path = __dirname + `/cache/${num += 1}.jpg`;
+        let getDown = (await axios.get(`${data[i]}`, { responseType: 'arraybuffer' })).data;
+        fs.writeFileSync(path, Buffer.from(getDown, 'utf-8'));
+        imgData.push(fs.createReadStream(__dirname + `/cache/${num}.jpg`));
     }
-  };
+
+    await api.sendMessage({
+        attachment: imgData,
+        body: "🥀Dalle Search Result💝\n\nPrompt: " + prompt + "\n\n#🌹Number of Images💓: " + numberSearch
+    }, event.threadID, event.messageID); 
+
+    for (let ii = 1; ii < parseInt(numberSearch); ii++) {
+        fs.unlinkSync(__dirname + `/cache/${ii}.jpg`);
+    }
+};
