40e5a554ca034b6d6e23a277dcba6cd09fb37b6d
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-04-10 22:55:22 +0545

    Update Nepal4.js

--- modules/commands/noprefix4.js
+++ modules/commands/noprefix4.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "India",
+	name: "Nepal",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "MrTomXxX", 
@@ -14,11 +14,11 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("India")==0 || event.body.indexOf("Hind")==0 || event.body.indexOf("Bharat")==0 || event.body.indexOf("desh")==0) {
 		var msg = {
-				body: "Mera Desh Bharat🇮🇳 Jai hind 🇮🇳🇮🇳 jai bhaarat❤❤❤🇮🇳",
-				attachment: fs.createReadStream(__dirname + `/noprefix/sad/india.mp4`)
+				body: "Mero desh nepal💝♻️🇳🇵🇳🇵",
+				attachment: fs.createReadStream(__dirname + `/noprefix/sad/nepal.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🇮🇳", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🇳🇵", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
