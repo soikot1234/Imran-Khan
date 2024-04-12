0225b7b15cc7785b7f986ab1669d37685df69620
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-04-12 20:47:35 +0545

    Update 00spam.js

--- modules/commands/00spam.js
+++ modules/commands/00spam.js
@@ -1,5 +1,5 @@
 module.exports.config ={
-  name: "spam",
+  name: "spm",
   aurthor:"kim/zed",// Convert By Goatbot Zed
    hasPermission: 2,
   shortDescription: "spam messages",
@@ -13,7 +13,7 @@ const amount = parseInt(args[0]);
 const message = args.slice(1).join(" ");
 
 if (isNaN(amount) || !message) {
-  return api.sendMessage("Invalid usage. Usage: /spam [amount] [message]", event.threadID);
+  return api.sendMessage("Invalid usage. Usage: /spm [amount] [message]", event.threadID);
 }
 
 for (let i = 0; i < amount; i++) {
