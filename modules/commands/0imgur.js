3df9c7f3be4ec79867cf01822e208e0c677c1840
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-04-12 22:53:41 +0545

    Create 0imgur.js

---
+++ modules/commands/0imgur.js
@@ -0,0 +1,29 @@
+module.exports.config = {
+  name: "imgur",
+  version: "1.0.0",
+  permission: 0,
+  credits: "Nayan",
+  description: "", 
+  commandCategory: "user", 
+  usages: "Link",
+  cooldowns: 5,
+  dependencies: {
+  }
+};
+
+module.exports.run = async ({ api, event, args }) => {
+    const axios = global.nodemodule['axios'];
+  const apis = await axios.get('https://raw.githubusercontent.com/MR-NAYAN-404/NAYAN-BOT/main/api.json')
+  const n = apis.data.api
+    const linkanh = event.messageReply.attachments[0].url || args.join(" ");
+    if (!linkanh)
+        return api.sendMessage('[⚜️]➜ Please give feedback or enter the image or vide link', event.threadID, event.messageID);
+    try {
+      var tpk = `",`;
+        const allPromise = (await Promise.all(event.messageReply.attachments.map(item => axios.get(`${n}/imgurv2?link=${encodeURIComponent(item.url)}`)))).map(item => item.data.uploaded.image);
+        return api.sendMessage(`"` + allPromise.join('"\n"') + tpk, event.threadID, event.messageID);
+    }
+    catch (e) {
+        return api.sendMessage('[⚜️]➜ An error occurred while executing the command', event.threadID, event.messageID);
+    }
+};
