893ddb6c59f8053da11a4f98b41633f188628289
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-04-14 00:42:41 +0545

    Create 0Imgurnopre.js

---
+++ modules/commands/0Imgurnopre.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+module.exports = {
+  config:{
+	name: "npx2",
+        version: "1.0.1",
+	permssion: 0,
+	credits: "nayan", 
+	description: "Fun",
+	commandCategory: "no prefix",
+	usages: "😒",
+        cooldowns: 5, 
+},
+
+handleEvent: async function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+  const axios = require('axios')
+const media = (
+    await axios.get(
+      '"https://i.imgur.com/frd8NTe.mp4",',
+      { responseType: 'stream' }
+    )
+  ).data;
+
+	if (body.indexOf("pir")==0 || body.indexOf("❤️‍🔥")==0 || body.indexOf("😢")==0 || body.indexOf("💔")==0 || body.indexOf("😭")==0 || body.indexOf("I love u")==0 || body.indexOf("sad")==0 || body.indexOf("pida")==0 || body.indexOf("Valobashi")==0 || body.indexOf("🖤")==0) {
+		var msg = {
+				body: "yestai xa🖤",
+				attachment: media
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("🖤", event.messageID, (err) => {}, true)
+		}
+	},
+	start: function({ nayan }) {
+  }
+}
