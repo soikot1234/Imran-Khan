e65e37e7402949879b9de4c62aabe163f0be482d
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-04-13 22:49:16 +0545

    Update 0drake.js

--- modules/commands/0drake.js
+++ modules/commands/0drake.js
@@ -2,12 +2,13 @@ module.exports.config = {
   name: "drake",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "mirai team",
-  description: "cover name maker",
-  commandCategory: "cover name",
-  usages: "text 1 + text 2",
-  cooldowns: 1
+  credits: "D-Jukie",
+  description: "drake",
+  commandCategory: "Tiện ích",
+  usages: "[text 1] | [text 2]",
+  cooldowns: 10
 };
+
 module.exports.wrapText = (ctx, text, maxWidth) => {
   return new Promise((resolve) => {
     if (ctx.measureText(text).width < maxWidth) return resolve([text]);
@@ -38,29 +39,30 @@ module.exports.wrapText = (ctx, text, maxWidth) => {
   });
 };
 
-module.exports.run = async function ({ api, event, args }) {
-  let { threadID, messageID } = event;
+module.exports.run = async function ({ api, event, args, Users }) {
+  let { senderID, threadID, messageID } = event;
   const { loadImage, createCanvas } = require("canvas");
   const Canvas = global.nodemodule["canvas"];
+  const request = require('request');
   const fs = global.nodemodule["fs-extra"];
   const axios = global.nodemodule["axios"];
-  let pathImg = __dirname + `/cache/dipto1.png`;
-  const text = args.join(" ").trim().replace(/\s+/g, " ").replace(/(\s+\=)/g, "+").replace(/\|\s+/g, "+").split("+");
+  let pathImg = __dirname + `/cache/drake.png`;
+  const text = args.join(" ").trim().replace(/\s+/g, " ").replace(/(\s+\|)/g, "|").replace(/\|\s+/g, "|").split("|");
   let getImage = (
-    await axios.get(encodeURI(`https://i.imgur.com/qmkwLUx.jpg`), {
+    await axios.get(encodeURI(`https://i.imgur.com/qmkwLUx.png`), {
       responseType: "arraybuffer",
     })
   ).data;
   fs.writeFileSync(pathImg, Buffer.from(getImage, "utf-8"));
-if(!fs.existsSync(__dirname+'/cache/dipto.ttf')) { 
-      let getfont = (await axios.get(`https://api.popcat.xyz/drake?text1=amongus&text2=amogus`, { responseType: "arraybuffer" })).data;
-       fs.writeFileSync(__dirname+"/cache/dipto.ttf", Buffer.from(getfont, "utf-8"));
+if(!fs.existsSync(__dirname+'/cache/SVN-Arial 2.ttf')) { 
+      let getfont = (await axios.get(`https://drive.google.com/u/0/uc?id=11YxymRp0y3Jle5cFBmLzwU89XNqHIZux&export=download`, { responseType: "arraybuffer" })).data;
+       fs.writeFileSync(__dirname+"/cache/SVN-Arial 2.ttf", Buffer.from(getfont, "utf-8"));
     };
   let baseImage = await loadImage(pathImg);
   let canvas = createCanvas(baseImage.width, baseImage.height);
   let ctx = canvas.getContext("2d");
   ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
-  Canvas.registerFont(__dirname+`/cache/dipto.ttf`, {
+  Canvas.registerFont(__dirname+`/cache/SVN-Arial 2.ttf`, {
         family: "SVN-Arial 2"
     });
   ctx.font = "30px SVN-Arial 2";
@@ -68,8 +70,8 @@ if(!fs.existsSync(__dirname+'/cache/dipto.ttf')) {
   ctx.textAlign = "center";
   const line = await this.wrapText(ctx, text[0], 464);
   const lines = await this.wrapText(ctx, text[1], 464);
-  ctx.fillText(line.join("\n"), 343, 135)
-  ctx.fillText(lines.join("\n"), 343, 339)
+  ctx.fillText(line.join("\n"), 464, 129)
+  ctx.fillText(lines.join("\n"), 464, 339)
   ctx.beginPath();
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
